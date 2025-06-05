3d2b38a7c8d57d85b8fa288aa4468e9ce9b2de6a
digwijaybpatil <digwijaybpatil@gmail.com>
2025-06-05 17:19:13 +0530

    added correct module path

--- todo-infra/main.tf
+++ todo-infra/main.tf
@@ -1,6 +1,6 @@
 module "rg_module" {
     
-  source = "modules/rg_module"
+  source = "./modules/rg_module"
   for_each = var.rgs
   resource_group_name     = each.value.name
   resource_group_location = each.value.location

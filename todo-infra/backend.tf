a931962d62e636b2b6d52ecbde90200f2ac65ea2
digwijaybpatil <digwijaybpatil@gmail.com>
2025-06-05 17:55:51 +0530

    code

--- todo-infra/backend.tf
+++ todo-infra/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "digwitfstatestorage"
-    container_name       = "tfstate"
-    key                  = "dev.tfstate"
-    subscription_id = "ab1ac19d-06cd-4ac2-b289-cbb67e904a92"
-  }
+  backend "azurerm" {} 
 }
